55d1f4df052f46bb97c728727d54ea02f83aa5c1
Uday Aravind <140690275+DUA-Designs@users.noreply.github.com>
2024-01-19 14:33:29 +0530

    Update script.js

--- script.js
+++ script.js
@@ -103,6 +103,7 @@ const smiley=[`<i class="fa-regular fa-face-smile-beam"></i>`,`<i class="fa-regu
 async function fetchCurrentData(){
 
     document.getElementById("questionForCurrent").style.display="none";
+    document.getElementById("errorForCurrent").style.display="none";
     flexBox.classList.remove("CollapseMe");
     moreDetails.classList.remove("makeVisible");
     await new Promise(resolve=>setTimeout(()=>setTimeout(()=>resolve("This is for loading Time"),1000)));
@@ -114,67 +115,15 @@ async function fetchCurrentData(){
 
    await new Promise(resolve=>setTimeout(()=>setTimeout(()=>resolve("This is for loading Time"),2000)));
  
-      const data={
-        "location": {
-            "name": "Pune",
-            "region": "Maharashtra",
-            "country": "India",
-            "lat": 18.53,
-            "lon": 73.87,
-            "tz_id": "Asia/Kolkata",
-            "localtime_epoch": 1705127713,
-            "localtime": "2024-01-13 12:05"
-        },
-        "current": {
-            "last_updated_epoch": 1705127400,
-            "last_updated": "2024-01-13 12:00",
-            "temp_c": 30.3,
-            "temp_f": 86.5,
-            "is_day": 1,
-            "condition": {
-                "text": "Sunny",
-                "icon": "//cdn.weatherapi.com/weather/64x64/day/113.png",
-                "code": 1000
-            },
-            "wind_mph": 2.2,
-            "wind_kph": 3.6,
-            "wind_degree": 107,
-            "wind_dir": "ESE",
-            "pressure_mb": 1014,
-            "pressure_in": 29.95,
-            "precip_mm": 0,
-            "precip_in": 0,
-            "humidity": 30,
-            "cloud": 5,
-            "feelslike_c": 29.2,
-            "feelslike_f": 84.6,
-            "vis_km": 10,
-            "vis_miles": 6,
-            "uv": 8,
-            "gust_mph": 6.7,
-            "gust_kph": 10.8,
-            "air_quality": {
-                "co": 841.1,
-                "no2": 14.4,
-                "o3": 137.3,
-                "so2": 26.2,
-                "pm2_5": 120.6,
-                "pm10": 131,
-                "us-epa-index": 4,
-                "gb-defra-index": 10
-            }
-        }
-    };
-
+     
     
       
     
-    /* const response=fetch(`https://api.weatherapi.com/v1/current.json?key=2f1a5f47063b4d3c96390406240201&q=${city.value}&aqi=${airCheck.value}`);
+     const response= await fetch(`https://api.weatherapi.com/v1/current.json?key=2f1a5f47063b4d3c96390406240201&q=${city.value}&aqi=${airCheck.value}`);
     
-     response.then((res)=>res.json()).then((data)=>{
-        console.log(data);
-        */
-       
+            const data=await response.json();
+        
+       if(data.current){
        locationHead.innerHTML=`City - ${data.location.name}, ${data.location.region} <span id="deg">${data.current.temp_c}<sup> o</sup><sub>c</sub></span>`;
        weatherIcon.src=`${data.current.condition.icon}`;
        temp.innerHTML=data.current.temp_c;
@@ -191,6 +140,10 @@ async function fetchCurrentData(){
        const timeCheck=data.location.localtime.split(" ")[1].split(":")[0];
 
        time.innerHTML=`${data.location.localtime} ${timeCheck>=12?"PM":"AM"}`;
+
+
+
+       
     if(airCheck.value==="yes"){
                coValue.innerHTML=data.current.air_quality.co;   
                no2Value.innerHTML=data.current.air_quality.no2;
@@ -200,6 +153,9 @@ async function fetchCurrentData(){
         let ind=data.current.air_quality["us-epa-index"];
        usEpa.innerHTML=data.current.air_quality["us-epa-index"];
        condition.innerHTML=`As per the "US - EPA standard" the condition seems to be "${conditionArray[ind>6?6:ind]}" ${smiley[ind>6?6:ind]}`;
+
+
+       
        document.getElementById("req").style.display="flex";
        document.getElementById("notReq").style.display="none";
 
@@ -210,18 +166,48 @@ async function fetchCurrentData(){
         document.getElementById("req").style.display="none";
        document.getElementById("notReq").style.display="block";
        }
+
        await new Promise(resolve=>setTimeout(()=>setTimeout(()=>resolve("This is for loading Time"),1000)));
        loader.style.display="none";
+
+       
+       flexBox.classList.add("CollapseMe");
+       setTimeout(()=>{
+       
+        moreDetails.classList.add("makeVisible");
+     },500); 
+   
+    }
+    else{
+
+       if(data.error.code===1003){
+        document.getElementById("errorForCurrent").innerHTML=`<h3 class="p-4 shadow rounded text-center">Kindly enter relevant details.</h3>`;
+       }
+       else{  
+        document.getElementById("errorForCurrent").innerHTML=`<h3 class="p-4 shadow rounded text-center">${data.error.message}</h3>`;
+
+       }
+
+
+
+        await new Promise(resolve=>setTimeout(()=>setTimeout(()=>resolve("This is for loading Time"),1000)));
+        loader.style.display="none";
+        document.getElementById("searchForCurrent").scrollIntoView({behavior:"smooth"});
+        document.getElementById("errorForCurrent").style.display="initial";
+
+       
+        setTimeout(()=>{
+        
+         moreDetails.classList.add("makeVisible");
+      },500); 
+    
+    }
+     
 
       
 
     
-     /*});*/
-     setTimeout(()=>{
-        flexBox.classList.add("CollapseMe");
-        moreDetails.classList.add("makeVisible");
-     },1000);
-    
+  
     
 }
 
